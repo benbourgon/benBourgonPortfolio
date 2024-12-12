8656d6f126173f29f85af0d8702fee173e53a3f0
Ben Bourgon <benjaminbourgon@gmail.com>
2024-12-12 10:14:35 -0500

    Remove extra dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig, passthroughImageService, envField } from "astro/config";
-import purgecss from "astro-purgecss";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
     output: "static",
     site: "https://www.benbourgon.com",
-    integrations: [purgecss(), icon(), mdx(), sitemap(), critters()],
+    integrations: [icon(), mdx(), sitemap()],
     image: {
         domains: ["res.cloudinary.com"],
         service: passthroughImageService(),
