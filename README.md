bed262c0c973dec573747a0663b096770ca4fa9c
Ben Bourgon <benjaminbourgon@gmail.com>
2025-07-14 18:56:31 -0400

    Remove project section and projects nav item

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Netlify Status](https://api.netlify.com/api/v1/badges/883dec41-8db2-4182-b06c-964e63361529/deploy-status)](https://app.netlify.com/sites/kind-wright-b01ef7/deploys)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,10 @@
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import yaml from "@rollup/plugin-yaml";
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
     output: "static",
     site: "https://www.benbourgon.com",
-    integrations: [icon(), mdx(), sitemap()],
-    vite: {
-        plugins: [yaml()],
-    },
-    env: {
-      schema: {
-        PUBLIC_CLOUDINARY_CLOUD_NAME: envField.string({
-          context: "client",
-          access: "public",
-        }),
-      }
-    },
+    integrations: [icon(), sitemap()],
 });
